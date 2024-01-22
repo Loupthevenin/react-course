a8612bb8bb5e8303980a38794a06d9623d00be97
AGENT MPR 2 <agent.mpr2@gd.local>
2024-01-22 11:38:43 +0100

    mobile styling

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import { v4 as uuidv4 } from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
 import Header from './components/Header';
-import Employees from './components/pages/Employees';
+import Employees from './pages/Employees';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Customers from './pages/Customers';
+
 
 function App() {
 
   return (
-    <Header>
-      <Employees />
-    </Header>
+        
+    <BrowserRouter>
+        <Header>
+            <Routes>
+                <Route path='/employees' element={<Employees />} />
+
+                <Route path='/customers' element={<Customers />} />
+            </Routes>
+        </Header>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -1,10 +1,10 @@
-import '../../index.css';
-import Employee from '../Employee';
+import '../index.css';
+import Employee from '../components/Employee';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import AddEmployee from '../AddEmployee';
-import EditEmployee from '../EditEmployee';
-import Header from '../Header';
+import AddEmployee from '../components/AddEmployee';
+import EditEmployee from '../components/EditEmployee';
+import Header from '../components/Header';
 
 function Employees() {
   const [role, setRole] = useState('dev');
@@ -73,10 +73,10 @@ function Employees() {
   const showEmployees = true;
 
   return (
-    <div className="App bg-gray-300 min-h-screen">
+    <div className="">
       {showEmployees ?
         <>
-          <div className='flex flex-wrap justify-center my-2'>
+          <div className='flex flex-wrap justify-center'>
             {employees.map((employee) => {
               const editEmployee = (<EditEmployee id={employee.id} name={employee.name} role={employee.role} updateEmployee={updateEmployee} />);
               return (<Employee key={employee.id} id={employee.id} name={employee.name} role={employee.role} img={employee.img} editEmployee={editEmployee} />);
