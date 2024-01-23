cdc7390df4dc45b496d5fe231ef997124fc41a63
AGENT MPR 2 <agent.mpr2@gd.local>
2024-01-23 13:40:58 +0100

    full crud

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { baseUrl } from "../shared";
+import AddCustomer from "../components/AddCustomer";
 
 export default function Customers() {
   const [customers, setCustomers] = useState();
+  const [show, setShow] = useState(false);
+
+  function toggleShow() {
+    setShow(!show);
+  }
 
   useEffect(() => {
     console.log("Fetching...");
@@ -14,18 +20,51 @@ export default function Customers() {
         setCustomers(data.customers);
       });
   }, []);
+  function newCustomer(name, industry) {
+    const data = { name: name, industry: industry };
+    const url = baseUrl + "api/customers/";
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        toggleShow();
+        setCustomers([...customers, data.customer]);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
   return (
     <>
       <h1>Here are our customer:</h1>
       {customers
         ? customers.map((customer) => {
             return (
-              <p>
-                <Link to={"/customers/" + customer.id}>{customer.name}</Link>
-              </p>
+              <div className="m-2" key={customer.id}>
+                <Link to={"/customers/" + customer.id}>
+                  <button className="no-underline bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+                    {customer.name}
+                  </button>
+                </Link>
+              </div>
             );
           })
         : null}
+      <AddCustomer
+        newCustomer={newCustomer}
+        show={show}
+        toggleShow={toggleShow}
+      />
     </>
   );
 }
